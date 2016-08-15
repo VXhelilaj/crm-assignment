8d9c89f6973a29016274965b6d185f190d1e03ee
viktor <v.xhelilaj@gmail.com>
2016-08-15 15:42:06 -0400

    updated all at once

--- contact.rb
+++ contact.rb
@@ -1,58 +1,84 @@
 class Contact
+#  vik = Contact.new("V","V","V","V")
+#  shane = Contact.new("S","S","S","S")
+  @@id = 1
+  @@contacts = []
 
+  attr_accessor :first_name, :last_name, :email, :note
+  attr_reader :id
   # This method should initialize the contact's attributes
-  def initialize
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
 
+    @@contacts << self
+    @id = @@id
+    @@id += 1
   end
 
-  # This method should call the initializer, 
+  # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name, last_name, email, note)
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
   end
+
 
   # This method should return all of the existing contacts
   def self.all
-
+    @@contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
-
+  def self.find(id)
+    @@contacts.each do |c|
+    return c if id == c.id
+    end
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
-  def update
-
+  def update(attribute, new_value)
+    case attribute
+    when "first name" then self.first_name = new_value
+    when "last name" then self.last_name = new_value
+    when "email" then self.email = new_value
+    when "note" then self.note = new_value
+   end
   end
 
   # This method should work similarly to the find method above
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
+  def self.find_by(first_name)
+    @@contacts.each do |c|
+      return c if first_name == c.first_name
+    end
 
   end
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    @@contacts.each {|c| @@contacts.clear}
+    end
   end
 
   def full_name
-
+    "#{first_name} #{last_name}"
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
+    @@contacts.delete(self)
   end
 
+
   # Feel free to add other methods here, if you need them.
-  
-end
--- crm.rb
+++ crm.rb
@@ -1,50 +1,18 @@
+require_relative "contact"
+
 class CRM
 
-  def initialize
+def initialize(name)
 
-  end
+  @name = name
 
-  def main_menu
+  puts "Okay, this CRM has the name " + name
+end
 
-  end
 
-  def print_main_menu
+def initialize(name_being_passed_in)
+  @name = name_being_passed_in
+end
 
-  end
-
-  def call_option
-
-  end
-
-  def add_new_contact
-
-  end
-
-  def modify_existing_contact
-
-  end
-
-  def delete_contact
-
-  end
-
-  # This method should accept as an argument an array of contacts
-  # and display each contact in that array
-  def display_contacts
-
-    # HINT: Make use of this method in the display_all_contacts and search_by_attribute methods to keep your code DRY
-  end
-
-  def display_all_contacts
-    
-    # HINT: Make use of the display_contacts method to keep your code DRY
-  end
-
-  def search_by_attribute
-    
-    # HINT: Make use of the display_contacts method to keep your code DRY
-  end
-
-  # Add other methods here, if you need them.
 
 end
