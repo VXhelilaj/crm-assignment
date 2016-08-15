e35f1ece7e9246f620cc741c4a99223dd8388d19
viktor <v.xhelilaj@gmail.com>
2016-08-15 15:56:28 -0400

    added the pre-done fields from the assignment page

--- crm.rb
+++ crm.rb
@@ -1,18 +1,80 @@
-require_relative "contact"
-
 class CRM
 
-def initialize(name)
+  def initialize
 
-  @name = name
+  end
 
-  puts "Okay, this CRM has the name " + name
-end
+  def main_menu
+    while true # repeat indefinitely
+        print_main_menu
+        user_selected = gets.to_i
+        call_option(user_selected)
+      end
+  end
 
+  def print_main_menu
+      puts '[1] Add a new contact'
+      puts '[2] Modify an existing contact'
+      puts '[3] Delete a contact'
+      puts '[4] Display all the contacts'
+      puts '[5] Search by attribute'
+      puts '[6] Exit'
+      puts 'Enter a number: '
+  end
 
-def initialize(name_being_passed_in)
-  @name = name_being_passed_in
-end
+  def call_option(user_selected)
+    case user_selected
+    when 1 then add_new_contact
+    when 2 then modify_existing_contact
+      # Finish off the rest for 3 through 6
+      # To be clear, the methods add_new_contact and modify_existing_contact
+      # haven't been implemented yet
+    end
+  end
 
+  end
+
+  def add_new_contact
+      print 'Enter First Name: '
+      first_name = gets.chomp
+
+      print 'Enter Last Name: '
+      last_name = gets.chomp
+
+      print 'Enter Email Address: '
+      email = gets.chomp
+
+      print 'Enter a Note: '
+      note = gets.chomp
+
+      Contact.create(first_name, last_name, email, note)
+  end
+
+  def modify_existing_contact
+
+  end
+
+  def delete_contact
+
+  end
+
+  # This method should accept as an argument an array of contacts
+  # and display each contact in that array
+  def display_contacts
+
+    # HINT: Make use of this method in the display_all_contacts and search_by_attribute methods to keep your code DRY
+  end
+
+  def display_all_contacts
+
+    # HINT: Make use of the display_contacts method to keep your code DRY
+  end
+
+  def search_by_attribute
+
+    # HINT: Make use of the display_contacts method to keep your code DRY
+  end
+
+  # Add other methods here, if you need them.
 
 end
